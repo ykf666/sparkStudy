45f47497861ee5e5b580c3e4c0b3bbba8a9c6ddf
yan.kefei <yan.kefei@opg.cn>
2017-06-30 19:23:52 +0800

    first spark program

--- build.sbt
+++ build.sbt
@@ -3,4 +3,6 @@ name := "SparkStudy"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.1.0")
     
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by yankefei on 2017/6/27.
+  */
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount")
+    val sc = new SparkContext(conf)
+
+    val input = sc.textFile("/data/testFile/helloSpark")
+    val lines = input.flatMap(line => line.split(" "))
+    val count = lines.map(word => (word, 1)).reduceByKey((a, b) => a + b)
+
+    val output = count.saveAsTextFile("/data/testFile/sparkRes")
+  }
+}
