854fccad4d4ce63024e838f71d4f789d8f8097b5
yan.kefei <yan.kefei@opg.cn>
2017-09-05 18:19:53 +0800

    SimpleDataOperations

--- build.sbt
+++ build.sbt
@@ -4,5 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.1.0")
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.1.0" % "provided",
+  "org.apache.spark" %% "spark-sql" % "2.1.0" % "provided"
+)
     
---
+++ src/main/scala/SimpleDataOperations.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by yankefei on 2017/9/5.
+  */
+class SimpleDataOperations {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("SimpleDataOperations").getOrCreate()
+
+    val url = "jdbc:mysql://10.60.1.248:3306/test"
+    val df = spark
+      .read
+      .format("jdbc")
+      .option("url", url)
+      .option("dbtable", "t_spark")
+      .option("user", "root")
+      .option("password", "root123")
+      .load()
+
+    // Looks the schema of this DataFrame.
+    df.printSchema()
+
+    // Counts people by age
+    val countsByAge = df.groupBy("age").count()
+    countsByAge.show()
+
+    // Saves countsByAge to S3 in the JSON format.
+    //    countsByAge.write.format("json").save("s3a://...")
+  }
+
+}
